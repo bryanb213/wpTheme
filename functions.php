fe763839e32a69881d698afd02e9973f4dc44e1e
bryanb213 <blancasbryan@gmail.com>
2019-11-01 17:20:51 -0500

    first commit

---
+++ functions.php
@@ -0,0 +1,10 @@
+<!-- global functions -->
+<?php
+function uniFiles() {
+    wp_enqueue_script('main-uni-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
+    wp_enqueue_style('custom-google-font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awseome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('uniMainStyles', get_stylesheet_uri(), NULL, microtime());
+}
+// before WP moves on run these functions
+add_action('wp_enqueue_scripts', 'uniFiles');
---
+++ single.php
@@ -0,0 +1,10 @@
+<!-- FOR WP POST CONTENT -->
+<?php
+    while(have_posts()){
+        the_post(); ?>
+        <h2><?php the_title(); ?></h2>
+        <h2><?php the_content(); ?></h2>
+        <?php
+    }
+    
+?>
