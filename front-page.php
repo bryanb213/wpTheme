25a962d693747ee0ee1965da6bc5a9788a8c2915
bryanb213 <blancasbryan@gmail.com>
2019-11-10 14:35:21 -0600

    made events dynamic

--- front-page.php
+++ front-page.php
@@ -14,27 +14,29 @@
     <div class="full-width-split__one">
         <div class="full-width-split__inner">
             <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
+            <?php
+            $homepageEvent = new WP_Query(array(
+                'post_type' => 'event',
+                'posts_per_page' => 2
+            ));
 
-            <div class="event-summary">
-                <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month">Mar</span>
-                    <span class="event-summary__day">25</span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
-                    <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
+            while ($homepageEvent->have_posts()) {
+                $homepageEvent->the_post(); ?>
+
+                <div class="event-summary">
+                    <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+                        <span class="event-summary__month"><?php the_time('M') ?></span>
+                        <span class="event-summary__day"><?php the_time('d') ?></span>
+                    </a>
+                    <div class="event-summary__content">
+                        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
+                        <p><?php echo wp_trim_words(get_the_content(), 15); ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                    </div>
                 </div>
-            </div>
-            <div class="event-summary">
-                <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month">Apr</span>
-                    <span class="event-summary__day">02</span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="#">Quad Picnic Party</a></h5>
-                    <p>Live music, a taco truck and more can found in our third annual quad picnic day. <a href="#" class="nu gray">Learn more</a></p>
-                </div>
-            </div>
+
+            <?php }
+            ?>
+
 
             <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
 
@@ -46,23 +48,24 @@
             <?php $homepagePost = new WP_Query(array(
                 'post_per_page' => 2
             )); // -> means to look inside
-            while($homepagePost -> have_posts()) {
-                $homepagePost -> the_post(); ?>
+            while ($homepagePost->have_posts()) {
+                $homepagePost->the_post(); ?>
                 <div class="event-summary">
-                <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
-                    <span class="event-summary__month"><?php the_time('M') ?></span>
-                    <span class="event-summary__day"><?php the_time('d') ?></span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                    <p><?php echo wp_trim_words( get_the_content(), 15 ); ?><a href="<?php the_permalink(); ?>" class="nu gray">  Read more</a></p<a>
+                    <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
+                        <span class="event-summary__month"><?php the_time('M') ?></span>
+                        <span class="event-summary__day"><?php the_time('d') ?></span>
+                    </a>
+                    <div class="event-summary__content">
+                        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                        <p><?php echo wp_trim_words(get_the_content(), 15); ?><a href="<?php the_permalink(); ?>" class="nu gray"> Read more</a></p<a>
+                    </div>
                 </div>
-            </div>
-                <?php
-              //resets everything to clean up
-            } wp_reset_postdata();?>
+            <?php
+                //resets everything to clean up
+            }
+            wp_reset_postdata(); ?>
 
-            
+
 
             <p class="t-center no-margin"><a href="<?php echo site_url('/index.php/blog'); ?>" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
