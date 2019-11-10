793592f3dd7ddfd4aef4efd042cd1362b0210136
bryanb213 <blancasbryan@gmail.com>
2019-11-10 14:06:17 -0600

    merged

--- functions.php
+++ functions.php
@@ -17,22 +17,3 @@ function uniFeatures(){
 }
 
 add_action('after_setup_theme', 'uniFeatures');
-
-//FOR NEW POST TYPES
-
-
-//NEW EVENT POST
-function uni_post_types(){
-    register_post_type( 'event', array(
-        'public' => true,
-        'menu_icon' => 'dashicons-calendar',
-        'labels' => array(
-            'name' => 'Events',
-            'add_new_item' => 'Add New Event',
-            'edit_item' => 'Edit Event',
-            'all_items' => 'All Events',
-            'singular_name' => 'Event'
-        )
-    ) );
-}
-add_action( 'init', 'uni_post_types');
